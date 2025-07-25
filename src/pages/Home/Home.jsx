969f2d0016eb0334492e2a8b9156d2812a95f2be
Sumeya Muhidin <sumsum8336@gmail.com>
2025-07-25 15:13:43 +0300

    Added a home page and reordered components

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from '@/components/Banner/Banner'
+import Courses from '@/components/Courses/Courses'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Courses />
+    </>
+  )
+}
+
+export default Home
