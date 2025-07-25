0d9cd56a9903c1aecdf18d76a1bfe44892ff7ee7
Sumeya Muhidin <sumsum8336@gmail.com>
2025-07-25 15:26:17 +0300

    updated the app.jsx , added routes inprove styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+// import { Route, Routes } from 'react-router'
+import { Route, Routes } from 'react-router'
 import './App.css'
+import CourseDetails from './components/Courses/CourseDetails'
+import Layout from './components/Layout/Layout'
+import Home from './pages/Home/Home'
+
 
 function App() {
   return (
     <>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses/:id" element={<CourseDetails />} />
+        </Routes>
+      </Layout>
     </>
   )
 }
