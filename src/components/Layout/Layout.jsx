0e5819a9ee852aade7f792300b6ceba08cb7bf9e
Sumeya Muhidin <sumsum8336@gmail.com>
2025-07-29 10:14:37 +0300

    Improve style of the layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,13 +4,13 @@ import Footer from '../Footer/Footer'
 
 const Layout = ({children}) => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen ">
       <Header />
-      <main>
+      <main className="flex-grow">
         {children}
       </main>
       <Footer/>
-    </>
+    </div>
   )
 }
 
